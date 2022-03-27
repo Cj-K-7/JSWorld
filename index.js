bbc59456b91bb8817a8d293049e8d2006f2f34a1
Cj-K-7 <kllt7059@gmail.com>
2022-03-27 18:42:11 +0900

    JS calculator acc logic

--- index.js
+++ index.js
@@ -28,36 +28,42 @@ const calculatorBtns = [
   "+",
 ];
 
-const calculating = (first, operator, second) => {
-  let result;
-  switch (operator) {
-    case "+":
-      result = first + second;
-      break;
-    case "-":
-      result = first - second;
-      break;
-    case "/":
-      result = first / second;
-      break;
-    case "*":
-      result = first * second;
-      break;
-  }
-  console.log(first,second);
-  resultPanel.innerHTML = result
-  return result;
-};
+let records = { formulars: [""], results: [""] };
 
 const formularTrans = (formular) => {
   let string = formular;
-  if ((string + "").includes("×")) string = (formular + "").replace("×", "*");
-  if ((string + "").includes("÷")) string = (formular + "").replace("÷", "/");
-
+  if ((string + "").includes("×"))
+    string = (formular + "").replace(/[×]/g, "*");
+  if ((string + "").includes("÷"))
+    string = (formular + "").replace(/[÷]/g, "/");
+  if ((string + "").match(/^[+-/*]+/))
+    string = records.results[records.results.length-1]+string;
+    console.log(records.results)
+    console.log(string);
   const operators = (string + "").split(/[0-9]+/).filter((oper) => oper);
-  const numbers = (string + "").split(/[+-/*]/);
-    console.log(operators, numbers);
-  calculating(parseFloat(numbers[0]),operators[0],parseFloat(numbers[1]));
+  const numbers = (string + "").split(/[+-/*]+/).map((a) => parseFloat(a));
+
+  const result = numbers.reduce((acc, cur, i) => {
+    switch (operators[i - 1]) {
+      case "+":
+        acc += cur;
+        break;
+      case "-":
+        acc -= cur;
+        break;
+      case "/":
+        acc /= cur;
+        break;
+      case "*":
+        acc *= cur;
+        break;
+    }
+    return acc;
+  });
+
+  resultPanel.innerHTML = result;
+  records.formulars.push(operation.innerText);
+  records.results.push(resultPanel.innerHTML);
 };
 
 const onClick = (event) => {
@@ -70,8 +76,8 @@ const onClick = (event) => {
       operation.innerText = "";
       return;
     case "=":
-        formularTrans(operation.innerText);
-              operation.innerText = "";
+      formularTrans(operation.innerText);
+      operation.innerText = "";
       return;
   }
   operation.innerText += value;
